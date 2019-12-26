2415df00d97d669a86070c2947e0d18029274e38
unknown <gosarkushal@gmail.com>
2019-12-26 21:29:00 +0530

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  url:'http://18.218.186.26:3000',
+  // url:'http://localhost:3000',
+  url:'http://13.126.98.129:3000',
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  url:'http://18.218.186.26:3000',
+  // url:'http://localhost:3000',
+  url:'http://13.126.98.129:3000',
   production: false
 };
 
